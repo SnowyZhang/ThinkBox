755d6038dcea16b0c37b2fa6f95113084ca70935
snowy <1251046722@qq.com>
2024-10-30 16:47:55 +0800

    SpringBoot init

---
+++ src/main/java/com/snowy/thinkbox/ThinkBoxApplication.java
@@ -0,0 +1,13 @@
+package com.snowy.thinkbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snowy/thinkbox/ThinkBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowy.thinkbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
