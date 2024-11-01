4f3ef801ebed934bc81f0560c5ae07c7422590f2
snowy <1251046722@qq.com>
2024-11-01 17:43:43 +0800

    增加HttpClient测试接口

--- src/main/java/com/snowy/thinkbox/controller/TestController.java
+++ src/main/java/com/snowy/thinkbox/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.snowy.thinkbox.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +9,9 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String postHello(String name){
+        return "Hello World!"+name;
     }
 }
