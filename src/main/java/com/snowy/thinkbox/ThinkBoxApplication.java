2a24a3605892f427063ee32920026767bb9e6e2b
snowy <1251046722@qq.com>
2024-11-01 16:10:02 +0800

    增加启动文案

--- src/main/java/com/snowy/thinkbox/ThinkBoxApplication.java
+++ src/main/java/com/snowy/thinkbox/ThinkBoxApplication.java
@@ -1,13 +1,20 @@
 package com.snowy.thinkbox;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ThinkBoxApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(ThinkBoxApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(ThinkBoxApplication.class, args);
+        SpringApplication app = new SpringApplication(ThinkBoxApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
