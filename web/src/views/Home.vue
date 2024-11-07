efe5a295aac9935fc8c51b12f5332f1278909a52
Snowy <zhangxsnowy@outlook.com>
2024-11-07 20:28:33 +0800

    加入Ant Design Vue list组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,18 +48,60 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-        {{ebooks}}
-        {{books}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
-import {LaptopOutlined, NotificationOutlined, UserOutlined} from "@ant-design/icons-vue"; // @ is an alias to /src
+import {LaptopOutlined, NotificationOutlined, UserOutlined,StarOutlined, LikeOutlined, MessageOutlined} from "@ant-design/icons-vue"; // @ is an alias to /src
 import axios from 'axios';
+
+const listData: Record<string, string>[] = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   name: 'Home',
   setup(){
@@ -78,7 +120,19 @@ export default defineComponent({
     });
     return {
       ebooks,
-      books:toRef(ebooksRef,"books")
+      books:toRef(ebooksRef,"books"),
+      listData,
+      pagination : {
+        onChange: (page: number) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { icon: StarOutlined, text: '156' },
+        { icon: LikeOutlined, text: '156' },
+        { icon: MessageOutlined, text: '2' },
+      ],
     }
   },
   components: {
