6d0a50b0b8be08f58faa5c0f6258af3fb65933e0
Snowy <zhangxsnowy@outlook.com>
2024-11-12 16:26:42 +0800

    增加AOP切面来打印接口耗时、请求参数和响应参数。因此，去掉拦截器和过滤器

--- src/main/java/com/snowy/thinkbox/config/SpringMvcConfig.java
+++ src/main/java/com/snowy/thinkbox/config/SpringMvcConfig.java
@@ -1,17 +1,17 @@
-package com.snowy.thinkbox.config;
-
-import com.snowy.thinkbox.interceptor.LogInterceptor;
-import jakarta.annotation.Resource;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    @Resource
-    private LogInterceptor logInterceptor;
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.snowy.thinkbox.config;
+//
+//import com.snowy.thinkbox.interceptor.LogInterceptor;
+//import jakarta.annotation.Resource;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//    @Resource
+//    private LogInterceptor logInterceptor;
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
