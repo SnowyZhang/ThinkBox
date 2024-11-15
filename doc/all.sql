c647e4199d89ad707a2d9018fba09079354c26bd
Snowy <zhangxsnowy@outlook.com>
2024-11-15 19:57:12 +0800

    设计并创建分类表,用mybatis-generator生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -25,3 +25,39 @@ CREATE TABLE `demo` (
                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='测试';
 insert into `demo` (id,name) values (1,'测试');
+
+CREATE TABLE `category` (
+  `id` BIGINT NOT NULL COMMENT 'id',
+  `name` VARCHAR(50) COMMENT '名称',
+  `parent_id` BIGINT COMMENT '父分类id',
+  `priority` int COMMENT '优先级',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='分类';
+-- 新增技术相关分类
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (500, '后端', 000, 401);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (501, 'Java', 500, 402);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (502, 'Python', 500, 403);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (503, 'Node.js', 500, 404);
+
+-- 新增框架相关分类
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (600, '框架', 000, 501);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (601, 'Django', 502, 502);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (602, 'SpringCloud', 101, 601);
+
+-- 新增工具类分类
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (700, '工具', 000, 601);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (701, 'Git', 700, 701);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (702, 'Docker', 700, 702);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (703, 'Kubernetes', 700, 703);
+
+-- 新增大数据分类
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (800, '大数据', 000, 801);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (801, 'Hadoop', 800, 802);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (802, 'Spark', 800, 803);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (803, 'Flink', 800, 804);
+
+-- 新增人工智能分类
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (900, '人工智能', 000, 901);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (901, '机器学习', 900, 902);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (902, '深度学习', 900, 903);
+INSERT INTO `category`(id, name, parent_id, priority) VALUES (903, '自然语言处理', 900, 904);
