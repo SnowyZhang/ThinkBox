ba77d6382398a74573f7a2fab1334745fb5d0e85
Snowy <zhangxsnowy@outlook.com>
2024-11-22 15:44:23 +0800

    新增文档显示界面（内容显示暂未完成），在首页点击电子书名称跳转到对应界面

---
+++ web/src/views/doc.vue
@@ -0,0 +1,10 @@
+<template>
+  <a-layout>
+    <a-layout-content
+      :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+    <div class="doc">
+      这是文档展示界面
+    </div>
+    </a-layout-content>
+  </a-layout>
+</template>
