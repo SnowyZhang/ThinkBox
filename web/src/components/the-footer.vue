597f46a1beb1a3cffa8c8cd1850a666f906febc7
Snowy <zhangxsnowy@outlook.com>
2024-12-07 15:25:01 +0800

    使用vuex设置全局变量，并在the-footer组件中测试使用

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,14 +1,19 @@
 <template>
   <a-layout-footer style="text-align: center">
     ThinkBox © 2024 | 赋能知识，助力成长
-    <!--      ThinkBox 团队倾心打造 | 意见反馈：zhangxsnowy@outlook.com-->
     「学习 · 构建 · 分享」
   </a-layout-footer>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import {useRef} from "preact/compat";
+import store from "@/store";
 
 export default defineComponent({
   name: 'the-footer',
+  setup () {
+    const user = computed(() => store.state.user);
+    return { user };
+  }
 });
 </script>
