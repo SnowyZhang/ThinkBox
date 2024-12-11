082d9e92aba256dc4d7ae852fdbbab06f956846d
Snowy <zhangxsnowy@outlook.com>
2024-12-11 21:15:07 +0800

    修改前端用户管理界面，与现在的user表相一致

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -6,7 +6,7 @@
       <p>
         <a-form layout="inline" :model="param">
           <a-form-item>
-            <a-input v-model:value="param.loginName" placeholder="登陆名">
+            <a-input v-model:value="param.email" placeholder="登录邮箱">
             </a-input>
           </a-form-item>
           <a-form-item>
@@ -14,11 +14,11 @@
               查询
             </a-button>
           </a-form-item>
-          <a-form-item>
-            <a-button type="primary" @click="add()">
-              新增
-            </a-button>
-          </a-form-item>
+<!--          <a-form-item>-->
+<!--            <a-button type="primary" @click="add()">-->
+<!--              新增-->
+<!--            </a-button>-->
+<!--          </a-form-item>-->
         </a-form>
       </p>
       <a-table
@@ -30,8 +30,8 @@
           @change="handleTableChange"
       >
         <template v-slot:headerCell="{ column }">
-          <template v-if="column.dataIndex === 'loginName'">
-            登陆名
+          <template v-if="column.dataIndex === 'email'">
+            登录邮箱
           </template>
           <template v-else-if="column.dataIndex === 'name'">
             名称
@@ -39,19 +39,31 @@
           <template v-else-if="column.dataIndex === 'password'">
             密码
           </template>
+          <template v-else-if="column.dataIndex === 'role'">
+            账户级别
+          </template>
+          <template v-else-if="column.dataIndex === 'isActive'">
+            激活状态
+          </template>
           <template v-else-if="column.key === 'action'">
             Action
           </template>
         </template>
         <template v-slot:bodyCell="{ column, record }">
-          <template v-if="column.dataIndex === 'loginName'">
-            {{ record.loginName }}
+          <template v-if="column.dataIndex === 'email'">
+            {{ record.email }}
           </template>
           <template v-else-if="column.dataIndex === 'name'">
             {{ record.name }}
           </template>
           <template v-else-if="column.dataIndex === 'password'">
             {{ record.password }}
+          </template>
+          <template v-else-if="column.dataIndex === 'role'">
+            {{ record.role }}
+          </template>
+          <template v-else-if="column.dataIndex === 'isActive'">
+            {{record.isActive==1 ? '已激活' : '未激活' }}
           </template>
           <template v-else-if="column.key === 'action'">
             <a-space size="small">
@@ -85,14 +97,20 @@
       @ok="handleModalOk"
   >
     <a-form :model="user" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-      <a-form-item label="登陆名">
-        <a-input v-model:value="user.loginName" :disabled="!!user.id"/>
+      <a-form-item label="登录邮箱">
+        <a-input v-model:value="user.email" :disabled="!!user.id"/>
       </a-form-item>
       <a-form-item label="昵称">
         <a-input v-model:value="user.name" />
       </a-form-item>
+      <a-form-item label="权限" >
+        <a-input v-model:value="user.role"/>
+      </a-form-item>
       <a-form-item label="密码" v-show="!user.id">
         <a-input v-model:value="user.password"/>
+      </a-form-item>
+      <a-form-item label="激活状态" v-show="!user.id">
+        <a-input v-model:value="user.isActive" />
       </a-form-item>
     </a-form>
   </a-modal>
@@ -136,8 +154,8 @@ export default defineComponent({
 
     const columns = [
       {
-        title: '登陆名',
-        dataIndex: 'loginName'
+        title: '登录邮箱',
+        dataIndex: 'email'
       },
       {
         title: '名称',
@@ -146,6 +164,14 @@ export default defineComponent({
       {
         title: '密码',
         dataIndex: 'password'
+      },
+      {
+        title: '账户级别',
+        dataIndex: 'role'
+      },
+      {
+        title: '激活状态',
+        dataIndex: 'isActive'
       },
       {
         title: 'Action',
@@ -165,7 +191,7 @@ export default defineComponent({
         params: {
           page: params.page,
           size: params.size,
-          loginName: param.value.loginName
+          email: param.value.email
         }
       }).then((response) => {
         loading.value = false;
@@ -230,10 +256,10 @@ export default defineComponent({
     /**
      * 新增
      */
-    const add = () => {
-      modalVisible.value = true;
-      user.value = {};
-    };
+    // const add = () => {
+    //   modalVisible.value = true;
+    //   user.value = {};
+    // };
 
     const handleDelete = (id: string) => {
       axios.post("/user/delete/" + id).then((response) => {
@@ -301,7 +327,7 @@ export default defineComponent({
       handleQuery,
 
       edit,
-      add,
+      // add,
 
       user,
       modalVisible,
